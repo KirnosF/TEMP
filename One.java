7d84a9d9d8e163f422b1c52d2d027c5cae42d650
Sergiy Kirnos <kirnosserhii@gmail.com>
2017-05-02 21:11:09 +0300

    First add file

---
+++ One.java
@@ -0,0 +1,8 @@
+/* <applet code="One" width=200 height=100></applet>> */
+import java.applet.*;
+import java.awt.*;
+public class One extends Applet {
+	public void paint(Graphics g) {
+	g.drawString("TEST", 25, 50);
+	}
+}
